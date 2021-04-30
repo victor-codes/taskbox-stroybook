d32da6f0edc481fb800097d9017becd909d8906f
AYODEJI <ayodejiv5@gmail.com>
2021-04-30 18:41:23 +0100

    Created TaskList.js, TaskList.stories.js, TaskList.test.js. Ran all the test none failed

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -3,35 +3,35 @@ import React from "react";
 import Task from "./Task";
 
 export default {
-    component: Task,
-    title: "Task"
-}
+  component: Task,
+  title: "Task",
+};
 
-const Template = args => <Task {...args} />
-export const Default = Template.bind({})
+const Template = (args) => <Task {...args} />;
+export const Default = Template.bind({});
 
 Default.args = {
-    task: {
-        id: "1",
-        title: "Test Task",
-        state: "TASK_INBOX",
-        updatedAt : new Date(2021, 0, 1, 9, 0)
-    },
-}
+  task: {
+    id: "1",
+    title: "Test Task",
+    state: "TASK_INBOX",
+    updatedAt: new Date(2021, 0, 1, 9, 0),
+  },
+};
 
-export const Pinned = Template.bind({})
+export const Pinned = Template.bind({});
 Pinned.args = {
-    task: {
-        ...Default.args.task,
-        state: "TASK_PINNED"
-    },
-}
+  task: {
+    ...Default.args.task,
+    state: "TASK_PINNED",
+  },
+};
 
-export const Archived = Template.bind({})
+export const Archived = Template.bind({});
 
-Archived.args= {
-    task: {
-        ...Default.args.task,
-        state: "TASK_ARCHIVED"
-    },
-}
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_ARCHIVED",
+  },
+};
